a16f1cfb8c1cd234ad87494d776b4340a3d3a356
Julian Uphoff <julianuphoff@users.noreply.github.com>
2019-10-09 11:51:04 +0200

    Improve system logging

---
+++ cloud/xenon/projectnamegenerator/Logger.java
@@ -0,0 +1,456 @@
+package generated.aea.iom;
+
+import helpers.Config;
+import helpers.Context;
+import java.util.*;
+import java.util.logging.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.IOException;
+
+
+public class ClsOvjtnlwnmq
+{
+	 public static final int classId = 316;
+	 static final Logger logger = LoggerFactory.getLogger(ClsOvjtnlwnmq.class);
+
+	public static void metKwdullewrqcq(Context context) throws Exception
+	{
+				int methodId = 0;
+		List<Object> root = new LinkedList<Object>();
+		Object[] valUennrpfdzoh = new Object[3];
+		Map<Object, Object> valClxoywvjbvz = new HashMap();
+		int mapValNhbimtxaazz = 10;
+		
+		int mapKeyPdplzzwzlpu = 917;
+		
+		valClxoywvjbvz.put("mapValNhbimtxaazz","mapKeyPdplzzwzlpu" );
+		
+		    valUennrpfdzoh[0] = valClxoywvjbvz;
+		for (int i = 1; i < 3; i++)
+		{
+		    valUennrpfdzoh[i] = Config.get().getRandom().nextInt(1000);
+		}
+		
+		
+		root.add(valUennrpfdzoh);
+		Set<Object> valTdbnbxpczzl = new HashSet<Object>();
+		List<Object> valUokuflwklyt = new LinkedList<Object>();
+		boolean valQcjyborpytg = true;
+		
+		valUokuflwklyt.add(valQcjyborpytg);
+		int valZhbkiwhyloa = 452;
+		
+		valUokuflwklyt.add(valZhbkiwhyloa);
+		
+		valTdbnbxpczzl.add(valUokuflwklyt);
+		Set<Object> valVcwtcfprfqt = new HashSet<Object>();
+		String valYhyerrdxbao = "StrZsudirlniwr";
+		
+		valVcwtcfprfqt.add(valYhyerrdxbao);
+		long valHlhifuewtby = 7057235768444476799L;
+		
+		valVcwtcfprfqt.add(valHlhifuewtby);
+		
+		valTdbnbxpczzl.add(valVcwtcfprfqt);
+		
+		root.add(valTdbnbxpczzl);
+					if (Config.get().shouldWriteLogInfo(context))
+			{
+					logger.info("Time for log - info 7Kfqrooip 4Stpns 12Vwtzsrretlnkf 9Ourzlbhdsf 9Klqkdsawuj 6Xkcaxlw 11Orefzvsndtiz 9Zqryzehluq 8Vgwjjwfdc 7Vdmcinwh 7Yldxbzas 3Rsmt 4Omfhu 6Bmgafpl 12Rclrqisovudtu 3Rrdm 10Wkfzwtajwoc 9Fsxsqhbafi 6Ybpszbt ");
+					logger.info("Time for log - info 5Mnhegy 3Jwnp 11Pltwziswfvsx 9Iigyhtzmip 8Znwyaljed 5Rufisf 4Homcs 10Ssbafeylcru 10Dsnyhbyadnv 3Rwzp 4Plink 11Ugzfzidmyxsh 9Fsltkxsnzf 6Ixfvwpo 3Wqwo 3Syrb 5Xzqwfb 10Stbnshurjve 12Ncurimqsvibtn ");
+					logger.info("Time for log - info 5Bguycx 11Upypugddblcd 7Mmrphqfb 3Rmmq 3Lkly 8Joxigmlcd 9Bnsgcxcwlz 5Notaqa 9Memkwzioou 12Iqilahdtmayag 4Bggpj 9Mohdixqdbh 7Fmzsdity 6Vkqqpal 6Uimxeih 10Ebdteehfspi 7Jdkurunh 5Aekdsj 8Mgcolstpg 8Dryxtnnmo 6Kblcshh 12Xxvskmlcxyurz 12Fekcamldveaxz 5Dgtsht 10Fhzawthbzna 4Efmjc 8Fqiofvcck 6Zpojneb ");
+			}
+		
+			if (Config.get().shouldThrowSomething(methodId,classId))
+			{
+				if(Config.get().shouldThrow1000())
+				{
+					for (int i = 0; i < 1000; i++) 
+			{
+			   	try
+				  {
+				  	throw new IllegalStateException("Time for Illegal state exception, context is " + context);
+		  		}
+			   	catch (Exception e)
+				  {
+				  	e.printStackTrace();
+			   	}
+      }
+
+				}
+		
+				if (Config.get().shouldWriteLogWarn(context))
+				{
+					logger.warn("Time for log - warn 6Wfvfavu 8Ogcymgpjp 8Andkynjqe 9Ocflwqmbia 4Wdztc 3Ddbn 4Dkzda 12Vidzlcjilneis 9Khmmkiiees 11Lrawxnnvfzto 5Psvkcq 6Fedfuou 10Jimywtczqen 4Gnqcr 11Tkhdpcgvgies 5Nzsvwp 3Wzyu 8Rmqzstrpw 6Ndqvpqi 5Gsrwtp 3Harw 4Jpqvs ");
+					logger.warn("Time for log - warn 9Lwfosvqrze 3Twdb 11Rjgukgjznzlj 6Veesdjk 8Cnsswcita 3Nwzs 4Cxznb ");
+					logger.warn("Time for log - warn 3Vlmd 10Bgurfaxmwbb ");
+				}
+		
+				if (Config.get().shouldWriteLogError(context))
+				{
+					logger.error("Time for log - error 6Hjljhjj 8Jyqdijmdg 10Moigghzegjb 8Jdlonxsdw 12Ktplaldxrvmzn 11Vcoyjusgmtmg 4Ezbdf 9Uuuuumjzdn 12Kkyeldtgiqwje ");
+					logger.error("Time for log - error 11Pcoltdtmkcvj 12Hrledjqvkwvsj 7Nlqfqetr 8Ippqhbdyz 5Hcyhnz 4Bqpcq 4Fafwy 7Jnyzwcka 5Ipbbwg 3Mytw 9Xlzsauyysk 11Idchcolvinoc 6Mchufnn 12Abslygzseszsr 10Dgxsxemgmyi ");
+					logger.error("Time for log - error 3Fxhy 6Ovihvpx 9Cqlvblydzb 12Mdzasuimzudcb 8Uwjdghdvv 5Yqlwkp 12Hebcvaybsxgwx 11Dyyrovmvbijb 12Fkkdzrdfioqwd 8Nyzplbqor 5Hzihcg 10Sgwdiwqkxzt 9Jsesfgdyti 11Ceixyaaimlrj 6Tvysybm 7Hrhbpmbe 5Xcsglz 8Frefauqup 9Miheyglrdl 7Ewddovge 3Bnnl 10Hnngsdlotnv 8Owenvdvqh 6Lbsjdus 11Wbdgqzqprlih 6Rglzule 3Rwro 5Vqrdud 6Fhcknus 4Silme 11Zqjldqyzeqlh ");
+					logger.error("Time for log - error 9Tdmegdsahd 7Nsscxymr 3Gsym 5Aevyjb 4Icmgw 12Qjxuocgsmosxm 5Xlxgmm 5Nfiiou 10Icwpnnrkvbj 4Zfwzr 6Nacbbtn 4Hwhrn 9Mhvcprzzye 10Vppbweympvk 7Neanixru 5Heocgd 9Kszvfaodnu 9Jjdmzuzrqi 10Vpnbhiafrxx 4Yvxud 8Sijzeozmp 11Qcvblbnxanwa 12Aevzoqkceukcl 5Gbtqcq 10Ovjozkajnac 10Mgynnlnqvak 4Sktwu 3Gozk ");
+				}
+		
+				if (Config.get().shouldThrowIllegal(context))
+				{
+						throw new IllegalStateException("Time for Illegal state exception, context is " + context);
+
+				}
+		
+				if (Config.get().shouldThrowIO(context))
+				{
+						throw new IOException("Time for IO exception, context is " + context);
+
+				}
+		}
+		
+				if (Config.get().shouldSuicide())
+				{
+		System.out.println("shouldSuicide.. ");
+						 System.exit(0);
+				}
+		
+			if (Config.get().shouldRunAway(context))
+			{
+				return;
+			}
+		
+		Config.get().updateContext(context);
+		int methodToCall = Config.get().getRandom().nextInt(5);
+		
+		switch (methodToCall)
+		{
+			case (0): generated.exnrv.npnx.zyxb.ClsLffziac.metKrjqsyrbe(context); return;
+			case (1): generated.pgfis.zew.ikgq.ClsEgsupkocifpi.metYobadnrmqm(context); return;
+			case (2): generated.dpe.jvo.jwdtk.ClsKqcmvv.metOqhxfxoow(context); return;
+			case (3): generated.vna.rvrp.nspt.ClsOdrnbbdzkkwwtm.metNfrkflcsgv(context); return;
+			case (4): generated.ojyuy.hdvu.yij.nuf.ClsJtdiu.metUymohzkoe(context); return;
+		}
+				{
+			int loopIndex25373 = 0;
+			for (loopIndex25373 = 0; loopIndex25373 < 6746; loopIndex25373++)
+			{
+				try
+				{
+					Integer.parseInt("numHdyaxkqfrzs");
+				}
+				catch(NumberFormatException e) 
+				{
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	}
+
+
+	public static void metKmfjrmgcij(Context context) throws Exception
+	{
+				int methodId = 1;
+		Map<Object, Object> root = new HashMap();
+		Set<Object> mapValXbsfdacvisw = new HashSet<Object>();
+		List<Object> valHvcvbygcwvd = new LinkedList<Object>();
+		long valSzjowkqrsmg = -5832562553782763056L;
+		
+		valHvcvbygcwvd.add(valSzjowkqrsmg);
+		int valYpyzwhhpddo = 737;
+		
+		valHvcvbygcwvd.add(valYpyzwhhpddo);
+		
+		mapValXbsfdacvisw.add(valHvcvbygcwvd);
+		
+		Map<Object, Object> mapKeyRkgtttvrdfp = new HashMap();
+		Set<Object> mapValTvfbipmmqje = new HashSet<Object>();
+		long valKmtobftbuge = -8922210791905226692L;
+		
+		mapValTvfbipmmqje.add(valKmtobftbuge);
+		int valNlltbgdqonk = 473;
+		
+		mapValTvfbipmmqje.add(valNlltbgdqonk);
+		
+		Object[] mapKeyWoyaxhcwzpq = new Object[4];
+		long valPeemdfescym = -669258954848827763L;
+		
+		    mapKeyWoyaxhcwzpq[0] = valPeemdfescym;
+		for (int i = 1; i < 4; i++)
+		{
+		    mapKeyWoyaxhcwzpq[i] = Config.get().getRandom().nextInt(1000);
+		}
+		
+		
+		mapKeyRkgtttvrdfp.put("mapValTvfbipmmqje","mapKeyWoyaxhcwzpq" );
+		
+		root.put("mapValXbsfdacvisw","mapKeyRkgtttvrdfp" );
+					if (Config.get().shouldWriteLogInfo(context))
+			{
+					logger.info("Time for log - info 6Tcheunt 10Wultbqvivxn 6Wditbjr 3Wmnd 6Rxkokpm 6Hstqrfp 4Umifz 5Kgxrug 11Mdiptzwnrppo 3Sibu 12Sfjveeybiiatm 10Vjibbagwqie 5Daeysy 7Gztntsqz 5Tcvmmb 8Hpxmuxfjv 5Kavvkx 10Lipawlqiiea 7Brefsicu 9Jiwcdfzkgm 7Albrarvk 4Vbvcy 11Rkuofhsvekfw 6Xnztpkl 4Easok 6Kiijijj ");
+					logger.info("Time for log - info 7Balpjdbc 4Olsld 7Olhogavg 12Vknresqagywul 7Ezicyhbt 4Wzodq 3Gdhh 8Gjmwlclou 9Pustryssfp 4Aepwy 11Ghomdqofnxvy 7Wqmbnjhq 5Pgbuuo 6Zmemkjs 6Ilryguz ");
+			}
+		
+			if (Config.get().shouldThrowSomething(methodId,classId))
+			{
+				if(Config.get().shouldThrow1000())
+				{
+					for (int i = 0; i < 1000; i++) 
+			{
+			   	try
+				  {
+				  	throw new IllegalStateException("Time for Illegal state exception, context is " + context);
+		  		}
+			   	catch (Exception e)
+				  {
+				  	e.printStackTrace();
+			   	}
+      }
+
+				}
+		
+				if (Config.get().shouldWriteLogWarn(context))
+				{
+				}
+		
+				if (Config.get().shouldWriteLogError(context))
+				{
+					logger.error("Time for log - error 3Taog 5Ndifbn 8Givooscfz 5Jpfycq 7Hpketpli 4Udfcb 10Aihxtywxrsp 8Hamkfklts 8Nckdcequg ");
+				}
+		
+				if (Config.get().shouldThrowIllegal(context))
+				{
+						throw new IllegalStateException("Time for Illegal state exception, context is " + context);
+
+				}
+		
+				if (Config.get().shouldThrowIO(context))
+				{
+						throw new IOException("Time for IO exception, context is " + context);
+
+				}
+		}
+		
+				if (Config.get().shouldSuicide())
+				{
+		System.out.println("shouldSuicide.. ");
+						 System.exit(0);
+				}
+		
+			if (Config.get().shouldRunAway(context))
+			{
+				return;
+			}
+		
+		Config.get().updateContext(context);
+		int methodToCall = Config.get().getRandom().nextInt(5);
+		
+		switch (methodToCall)
+		{
+			case (0): generated.vbmu.nqy.tvok.ClsTqtbdjb.metNhjvsnqwnbit(context); return;
+			case (1): generated.unn.flt.iyr.tjmvm.ClsFuxljnbiciwosz.metQrptesizpvf(context); return;
+			case (2): generated.qjqzm.vzrn.zbzow.ClsDltypnpb.metQfyhues(context); return;
+			case (3): generated.uyus.ruq.fow.dvnw.ClsCysblmgtiyuxcr.metExmmbm(context); return;
+			case (4): generated.zkx.deuj.ClsQemyphqswqdyqm.metPinhqtobr(context); return;
+		}
+				{
+			if (((Config.get().getRandom().nextInt(864) + 5) + (Config.get().getRandom().nextInt(499) + 4) % 959332) == 0)
+			{
+				try
+				{
+					Integer.parseInt("numQpmmpwztsip");
+				}
+				catch(NumberFormatException e) 
+				{
+					e.printStackTrace();
+				}
+			}
+			else if (((Config.get().getRandom().nextInt(347) + 0) * (Config.get().getRandom().nextInt(927) + 1) % 537637) == 0)
+			{
+				java.io.File file = new java.io.File("/dirWanpicsrwph/dirHdwagiuzswg");
+				
+				if (file.canRead())
+				{
+					System.out.println("File exists");
+				}
+				else
+				{
+					System.out.println("File not exists");
+				}
+			}
+			else
+			{
+				try
+				{
+					Integer.parseInt("numOxuscmfiqpy");
+				}
+				catch(NumberFormatException e) 
+				{
+					e.printStackTrace();
+				}
+			}
+			
+			int loopIndex25377 = 0;
+			for (loopIndex25377 = 0; loopIndex25377 < 5892; loopIndex25377++)
+			{
+				java.io.File file = new java.io.File("/dirWiemofbtsif");
+				
+				if (file.canRead())
+				{
+					System.out.println("File exists");
+				}
+				else
+				{
+					System.out.println("File not exists");
+				}
+			}
+			
+		}
+	}
+
+
+	public static void metKhmaqmoiluaib(Context context) throws Exception
+	{
+				int methodId = 2;
+		Map<Object, Object> root = new HashMap();
+		Set<Object> mapValGxmiacncorf = new HashSet<Object>();
+		List<Object> valIxkxmmzjkhb = new LinkedList<Object>();
+		long valXiajetawkbj = 3068609328402299778L;
+		
+		valIxkxmmzjkhb.add(valXiajetawkbj);
+		long valHuneolkqqkb = 9105769025017442949L;
+		
+		valIxkxmmzjkhb.add(valHuneolkqqkb);
+		
+		mapValGxmiacncorf.add(valIxkxmmzjkhb);
+		Map<Object, Object> valJphylwuyhbg = new HashMap();
+		long mapValYvwphllgveh = -6908817189980461579L;
+		
+		long mapKeyMzsygxncikf = 4636657912590713827L;
+		
+		valJphylwuyhbg.put("mapValYvwphllgveh","mapKeyMzsygxncikf" );
+		
+		mapValGxmiacncorf.add(valJphylwuyhbg);
+		
+		Object[] mapKeyQmetujzcaxx = new Object[2];
+		Object[] valUjboaqglcva = new Object[11];
+		String valFwoufvsgmnd = "StrKcqxwxjvnmk";
+		
+		    valUjboaqglcva[0] = valFwoufvsgmnd;
+		for (int i = 1; i < 11; i++)
+		{
+		    valUjboaqglcva[i] = Config.get().getRandom().nextInt(1000);
+		}
+		
+		
+		    mapKeyQmetujzcaxx[0] = valUjboaqglcva;
+		for (int i = 1; i < 2; i++)
+		{
+		    mapKeyQmetujzcaxx[i] = Config.get().getRandom().nextInt(1000);
+		}
+		
+		
+		root.put("mapValGxmiacncorf","mapKeyQmetujzcaxx" );
+					if (Config.get().shouldWriteLogInfo(context))
+			{
+					logger.info("Time for log - info 4Rrmya 3Khok 4Fxcqs 7Woqaihxl 11Gpreqqhhvhpn 6Kzccucb 12Hvnxoltunezjb 8Lewdmzqty 7Onwstlrr 9Vkwktazich 7Mbkyouyz 6Sdfedyv 5Lgbnwj 6Jcyvulc 8Cepjnbdzb 5Guuydv 12Otznjfqlrgfvv 8Sqzoksokf 7Fahxenlc 12Opfgtuquafbrc ");
+					logger.info("Time for log - info 3Dbba 8Ewgbmyaoa ");
+			}
+		
+			if (Config.get().shouldThrowSomething(methodId,classId))
+			{
+				if(Config.get().shouldThrow1000())
+				{
+					for (int i = 0; i < 1000; i++) 
+			{
+			   	try
+				  {
+				  	throw new IllegalStateException("Time for Illegal state exception, context is " + context);
+		  		}
+			   	catch (Exception e)
+				  {
+				  	e.printStackTrace();
+			   	}
+      }
+
+				}
+		
+				if (Config.get().shouldWriteLogWarn(context))
+				{
+					logger.warn("Time for log - warn 11Qsrxqtaybsqs 5Cdatfs 5Lmyiio 12Usughaibdbgnq 8Zlykduixs 7Msxqrlfk 8Sdhzihgxg 3Hhtn 3Yrzx 11Xpunjkbulhpa 5Lmabez 9Ubnwkbwnpi 6Eijursl 7Ggdyzgab ");
+					logger.warn("Time for log - warn 4Qynkw 7Lszpaspb 12Yjieiihikzaaw 4Ojmsh 12Mmiyqtmfzhblv 12Gbhmyspqubbdf 5Whiwzq 9Eptjmfqmcg 11Itthipkjzjvi 4Nlqrk 11Oqcqlprwcqgi 12Yheevrtvjsmzc 9Vbcmcaftfh 7Bodmykwt 4Bovht 7Qvmwqmlx 9Yvgatgzsqp 10Lsnggrqphfb 3Ikdm 10Evdqelscfyh 5Ybcqkw 5Aqlexa ");
+					logger.warn("Time for log - warn 6Crermuj 9Pulbupjzgy 6Eigvaqe 5Sldhwb 3Vvjw 12Kbinhounvqcyr 8Yinhzzftw 10Hrhcsxzofgz 12Jkmiumueyyixr 8Tmkdhrssq 6Mfwwjhf 9Xfqulkocpm 6Dgridgl 6Llqqjxa 12Ttslvebdfsvhp 12Ttkrgblhtdvtg 10Dhnipkvyrvf 7Vbkhlpkt 7Bfewacid 10Nueixaygbkm 9Momxcjjzcf ");
+					logger.warn("Time for log - warn 7Nameqwln 12Rggacxwkxueov 6Fzzvnuy 9Rpydnkiwyc 11Imlhjiebzidq 8Svzyqkbrq 3Kpxr 12Oahmsyuknigts 10Qvvojphcoty 9Vppbajdjhp 6Rpzrclm 3Kewx ");
+				}
+		
+				if (Config.get().shouldWriteLogError(context))
+				{
+					logger.error("Time for log - error 6Uktsujj 5Abcwit 6Qwbefmj 3Njmy 11Jdymgvsrtako 8Vfsriehiy 9Ohvgnfbzxf 4Cakix 12Tlhyqcutpbazf 10Qhdftvrrayw 10Esscatwveze 12Nvuwezyimvghw 3Xhib 5Holpum 3Qeov 5Tahmfy 6Oyjfmfm 9Sbjrtxhdxv 4Srmta 11Yhzsyfzpnzli 4Tqdgb 7Ndfcaytt 9Hejjucbwog 9Omahcboqmw 4Gwwvo 8Kovvvqbos ");
+				}
+		
+				if (Config.get().shouldThrowIllegal(context))
+				{
+						throw new IllegalStateException("Time for Illegal state exception, context is " + context);
+
+				}
+		
+				if (Config.get().shouldThrowIO(context))
+				{
+						throw new IOException("Time for IO exception, context is " + context);
+
+				}
+		}
+		
+				if (Config.get().shouldSuicide())
+				{
+		System.out.println("shouldSuicide.. ");
+						 System.exit(0);
+				}
+		
+			if (Config.get().shouldRunAway(context))
+			{
+				return;
+			}
+		
+		Config.get().updateContext(context);
+		int methodToCall = Config.get().getRandom().nextInt(5);
+		
+		switch (methodToCall)
+		{
+			case (0): generated.ocklj.sbz.ClsVzertfboftzd.metOvqborsb(context); return;
+			case (1): generated.xvt.cmvm.fgj.efz.bawb.ClsIacmgbgh.metHijjkg(context); return;
+			case (2): generated.byxi.uzy.rgdf.nzlsj.fbg.ClsCwwdznsfknncdl.metNslqlokpu(context); return;
+			case (3): generated.avme.rnc.ClsUgqeozcqx.metIsegbgah(context); return;
+			case (4): generated.hcl.ivd.nxcam.ClsQkvrtnftml.metTyfahozhlcema(context); return;
+		}
+				{
+			long whileIndex25385 = 0;
+			
+			while (whileIndex25385-- > 0)
+			{
+				Object locker = new Object();
+				
+				synchronized (locker)
+				{
+					System.out.println("synchronized block");
+				}
+			}
+			
+			if (((whileIndex25385) % 794694) == 0)
+			{
+				java.io.File file = new java.io.File("/");
+				
+				if (file.canRead())
+				{
+					System.out.println("File exists");
+				}
+				else
+				{
+					System.out.println("File not exists");
+				}
+			}
+			
+		}
+	}
+
+}
